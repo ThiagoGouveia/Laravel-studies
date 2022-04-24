8edbd63cf0f36fc25ff6158c6975ca207684812f
ThiagoGouveia <thiago.rodrigues.gouveia@gmail.com>
2022-04-24 18:51:49 -0300

    one to one CRUD

--- oneToOne/routes/web.php
+++ oneToOne/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Address;
+use App\Models\User;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//INSERT
+Route::get('/insert', function () {
+    
+    $user = User::findOrFail(1);
+    $address = new Address(['name'=>'Rua alvaro leal calmon']);
+    $user->address()->save($address);
+});
+
+//UPDATE
+
+Route::get('/update', function () {
+    
+    $address = Address::where('user_id', 1)->first();
+    $address->name = "Alvaro Leal Calmon, 1238";
+    $address->save();  
+});
+
+//READ 
+
+Route::get('/read', function () {
+    
+    $user = User::findOrfail(1);
+    echo $user->address->name;
+});
+
+//DELETE
+
+Route::get('/delete', function () {
+    
+    $user = User::findOrfail(1);
+    $user->address()->delete();
+    
+});
+
